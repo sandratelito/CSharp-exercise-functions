9e14331133d03cae464acc6b01034dff946121e6
Sandra <sandra.stankaityte@gmail.com>
2019-02-04 19:59:31 +0200

    Max ir InRange formules

--- CSharpFunctions/CSharpFunctions/Program.cs
+++ CSharpFunctions/CSharpFunctions/Program.cs
@@ -4,9 +4,60 @@ namespace CSharpFunctions
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Max(a, b, c);
+            InRange(a, b, c);
         }
+
+        static int Max(int skaicius1, int skaicius2, int skaicius3)
+        {
+            if(skaicius1 > skaicius2 && skaicius1 > skaicius3)
+            {
+                Console.WriteLine(skaicius1);
+                return skaicius1;
+            } else if(skaicius2 > skaicius1 && skaicius2 > skaicius3)
+            {
+                Console.WriteLine(skaicius2);
+                return skaicius2;
+            }
+            else if(skaicius3 > skaicius1 && skaicius3 > skaicius2)
+            {
+                Console.WriteLine(skaicius3);
+                return skaicius3;
+            }
+            else
+            {
+                return 0;
+            }
+
+
+
+        }
+
+
+        static bool InRange(int skaicius1, int skaicius2, int skaicius3)
+        {
+            if(skaicius1 > skaicius2 && skaicius1 < skaicius3)
+            {
+                Console.WriteLine("true");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("false");
+                return false;
+            }
+        }
+
+
+
+
+
+
     }
 }
